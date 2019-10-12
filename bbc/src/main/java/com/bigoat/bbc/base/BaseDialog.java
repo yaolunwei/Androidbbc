66a0f2ed1971bb07dc5a21c5f7e910159d33dccf
yaolunwei <smallmonkey>
2019-10-12 10:51:18 +0800

    BaseDialog add show function.

--- bbc/src/main/java/com/bigoat/bbc/base/BaseDialog.java
+++ bbc/src/main/java/com/bigoat/bbc/base/BaseDialog.java
@@ -9,6 +9,7 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -186,4 +187,9 @@ public abstract class BaseDialog<Binding extends ViewDataBinding, ViewModel exte
         act.hideProgress();
     }
 
+    public void show(FragmentManager manager) {
+        if (!isAdded()) {
+            show(manager, getClass().getSimpleName());
+        }
+    }
 }
